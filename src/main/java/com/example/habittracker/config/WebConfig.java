203ef945a3bb3b52465ae4742eb619cfa9df0850
Manda Supraja <mandasupraja384@gmail.com>
2026-09-04 16:16:50 +0530

    Fix CORS for deployed frontend

--- src/main/java/com/example/habittracker/config/WebConfig.java
+++ src/main/java/com/example/habittracker/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.example.habittracker.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,10 +9,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("https://habit-tracker-project-9vuy.onrender.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+
+    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:C:/Users/LENOVO/Desktop/Habit Tracker front end/uploads/");
-
     }
 }
